df1f018a90253a950ced9b14b913769a873e8eda
unknown <kakarotoroth@gmail.com>
2023-07-05 05:39:48 -0400

    deploy done2

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,13 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'static',
-  outDir: './docs',
+  // output: 'static',
+  // outDir: './docs',
   site: 'https://jcarvajal1987.github.io',
   base:'/apr',
-  build: {
-    format: 'file',
-    assets: 'assests',
-    assetsPrefix: '.'
-  }
+  // build: {
+  //   format: 'file',
+  //   assets: 'assests',
+  //   assetsPrefix: '.'
+  // }
 });
